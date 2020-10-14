a27f904dfbb93c0c7f9e1a3d11562f46bb5832e8
Palex-lab <71607147+Palex-lab@users.noreply.github.com>
2020-10-14 20:04:20 +0200

    Add files via upload

---
+++ Dictionary.java
@@ -0,0 +1,26 @@
+public abstract class Dictionary {
+    
+    public static final int ESC = 27;
+    public static final int BACKSPACE = 127;
+    public static final int DERECHA = 67;
+    public static final int IZQUIERDA = 68;
+    public static final int ABAJO = 66;
+    public static final int HOME = 72;
+    public static final int END = 70;
+    public static final int INSERT = 65;
+    public static final int DELETE = 51;
+    public static final int CORCHETE = 91;
+    public static final int TILDE = 126; //Símbolo ~
+    public static final int ENTER = 13;
+    public static final int PAJARITO = 94; //Símbolo ^ 
+
+    public static final int FLECHA_DERECHA = 2001;
+    public static final int FLECHA_IZQUIERDA = 2002;
+    public static final int FLECHA_ARRIBA = 2003;
+    public static final int FLECHA_ABAJO = 2004;
+    public static final int TECLA_HOME = 2005;
+    public static final int TECLA_FIN = 2006;
+    public static final int TECLA_INSERT = 2007;
+    public static final int TECLA_DELETE = 2008;
+    public static final int TECLA_CARACTER = 2009;     
+}
--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -1,38 +1,115 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.io.IOException;
+import java.io.*;
+import java.lang.String;
 
 public class EditableBufferedReader extends BufferedReader {
+
+    private Line linea;
+
     public EditableBufferedReader(InputStreamReader in){
         super(in);
+        this.linea = new Line();
     }
-
+    
     public void setRaw(){
-    try{ 
-        String[] cmd = {"/bin/sh", "-c","stty -echo raw </dev/tty"};
-        Runtime.getRuntime().exec(cmd);
-    } catch(IOException e){
-        System.out.println("Error setting Raw mode");
-    }
+        try{
+            String[] cmd = {"/bin/sh","-c", "stty -echo raw </dev/tty"};
+            Runtime.getRuntime().exec(cmd);
+        } catch(IOException e) { System.out.println("Error setRaw()");} 
     }
 
     public void unsetRaw(){
-        try{ 
-            String[] cmd = {"/bin/sh", "-c","stty -echo cooked </dev/tty"};
+        try{
+            String[] cmd = {"/bin/sh","-c", "stty echo cooked </dev/tty"};
             Runtime.getRuntime().exec(cmd);
-        } catch(IOException e){
-            System.out.println("Error setting Cooked mode");
+        } catch(IOException e) { System.out.println("Error unsetRaw()");} 
+    }
+    
+    public int read() throws IOException {
+        int entrada = 0;
+        entrada = super.read();
+        if(entrada == Dictionary.ESC){
+            entrada = super.read();
+            if(entrada == Dictionary.CORCHETE){
+                switch(entrada) {
+                    case Dictionary.DERECHA:
+                        return Dictionary.FLECHA_DERECHA;
+                    case Dictionary.IZQUIERDA:
+                        return Dictionary.FLECHA_IZQUIERDA;
+                    case Dictionary.HOME:
+                        return Dictionary.TECLA_HOME;
+                    case Dictionary.END:
+                        return Dictionary.TECLA_FIN;
+                    case Dictionary.INSERT:
+                        return Dictionary.TECLA_INSERT;
+                    case Dictionary.DELETE: 
+                        entrada = super.read();
+                        if(entrada == Dictionary.TILDE){
+                            return Dictionary.TECLA_DELETE;
+                        }
+                        return -1;
+                    default:
+                        return -1;
+                }
+            }
+        }       
+        else if(entrada == Dictionary.BACKSPACE) {
+            return Dictionary.BACKSPACE;  
+        }   
+        else {
+            return entrada;
         }
-        }
+        return -1;
+    } 
 
-//    public void read(){
-//        try{ 
-//            Scanner sc = new Scanner(System.in);
-//            String nextCar = sc.next();
-//            if(nextCar)
-//        } catch(IOException e){
-//            System.out.println("Error reading character");
-//       }
-//        }
+    public String readLine() throws IOException {
+        this.setRaw();
+        int entrada = 0;
+        entrada = this.read();
+        while(entrada!= Dictionary.ENTER){
+            switch(entrada){
+                case Dictionary.FLECHA_DERECHA:
+                    linea.mover_derecha();
+                    //mover hacia la derecha
+                    break;
+                case Dictionary.FLECHA_IZQUIERDA:
+                    linea.mover_izquierda();
+                    //mover hacia la izquierda
+                    break;
+                case Dictionary.TECLA_HOME:
+                    linea.home();
+                    //ir al principio
+                    break;
+                case Dictionary.TECLA_FIN:
+                    linea.end();
+                    //ir al final
+                    break;
+                case Dictionary.TECLA_INSERT:
+                    linea.insertarMode();
+                    //insertar
+                    break;
+                case Dictionary.TECLA_DELETE:
+                    linea.delete();
+                    //borrar
+                    break;
+                case Dictionary.BACKSPACE:
+                    linea.backspace();
+                default: //otro caracter
+                char temp = (char) entrada;
+                linea.insertar_caracter(temp);
+                System.out.print(temp);
+                //linea.insertar_caracter((char) entrada);
+                    //añadirlo...
+
+                    break;
+            }
+            entrada = super.read(); 
+        }
+        //String str = linea.toString();
+        String str = linea.getBuffer().toString();
+        this.unsetRaw(); 
+        return str;
+    }
+
+    
+
 }
